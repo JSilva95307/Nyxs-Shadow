afe560d197bb73a110132b45d8484f764998ee26
Chewy <jsilvaisyourbro@gmail.com>
2024-12-21 21:18:08 -0600

    Updated Inputs
    Implemented the new input system, also added the needed inputs into the Player Controller.

---
+++ Assets/Scripts/Managers/GameManager.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    private static GameManager _instance;
+
+    public static GameManager Instance {  get { return _instance; } }
+
+    private void Awake()
+    {
+        if (_instance != null && _instance != this)
+        {
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            _instance = this;
+        }
+        DontDestroyOnLoad(this.gameObject);
+    }
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,7 +1,30 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
+    #region functionality Vars
+    bool canGoUpOrDown = false;
+    public Rigidbody2D rb;
+    public PlayerInputs controls;
+    #endregion
+
+    #region Inputs
+    InputAction move;
+    InputAction jump;
+    InputAction primary;
+    InputAction secondary;
+    InputAction ability1;
+    InputAction ability2;
+    #endregion
+
+    Vector2 playerVel = Vector2.zero;
+    // Awake executes only once you start to load the game
+    private void Awake()
+    {
+        controls = new PlayerInputs();
+    }
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -13,4 +36,18 @@ public class PlayerController : MonoBehaviour
     {
         
     }
+
+
+    #region Input Boilerplate
+    private void OnEnable()
+    {
+        
+    }
+
+    private void OnDisable()
+    {
+        
+    }
+    #endregion
+
 }
