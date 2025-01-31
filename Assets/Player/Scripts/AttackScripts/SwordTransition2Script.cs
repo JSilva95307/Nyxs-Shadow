9453c1cddc8c6cf8c3a52814ed763f3eef4ad32b
Xavier Williams <XMWilliams@student.fullsail.edu>
2025-01-31 00:01:21 -0500

    Implemented the third hit of the sword attack combo

--- Assets/Player/Scripts/AttackScripts/SwordTransition2Script.cs
+++ Assets/Player/Scripts/AttackScripts/SwordTransition2Script.cs
@@ -9,16 +9,19 @@ public class SwordTransition2Script : StateMachineBehaviour
     //}
 
     // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
-    //override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
-    //{
-    //    
-    //}
+    override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        if (AttackController.instance.isAttacking)
+        {
+            AttackController.instance.animator.Play("SwordAttack3");
+        }
+    }
 
     // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
-    //override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
-    //{
-    //    
-    //}
+    override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        AttackController.instance.isAttacking = false;
+    }
 
     // OnStateMove is called right after Animator.OnAnimatorMove()
     //override public void OnStateMove(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
