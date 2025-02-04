351d6f3dfd490267aab727fb0ce4aabb89f44e07
Chewy <jsilvaisyourbro@gmail.com>
2025-02-03 21:18:25 -0600

    States Traversal
    Implemented some of the first state transitions for the first mini boss, the forest spirit. Currently implemented and functioning are: Run and Primary attack. Attack does not do damage yet.

--- Assets/Bosses/Mini-Bosses/Sprites/TreeSpirit/Spirit_Walk.cs
+++ Assets/Bosses/Mini-Bosses/Sprites/TreeSpirit/Spirit_Walk.cs
@@ -2,21 +2,42 @@ using UnityEngine;
 
 public class Spirit_Walk : StateMachineBehaviour
 {
+
+    public float walkSpeed = 2.5f;
+    public float attackRange = 1f;
+
+    Transform player;
+    Rigidbody2D rb;
+    Boss boss;
+
+
     // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
     override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
-
+        // Get Player Transform;
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+        rb = animator.GetComponent<Rigidbody2D>();
+        boss = animator.GetComponent<Boss>();
     }
 
     // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
     override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
+        boss.LookAtPlayer();
 
+        Vector2 target = new Vector2(player.position.x, rb.position.y);
+        Vector2 newPos = Vector2.MoveTowards(rb.position, target, walkSpeed * Time.fixedDeltaTime);
+        rb.MovePosition(newPos);
+
+        if (Vector2.Distance(player.position, rb.position) <= attackRange)
+        {
+            animator.SetTrigger("Attack");
+        }
     }
 
     // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
     override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
-
+        animator.ResetTrigger("Attack");
     }
 }
