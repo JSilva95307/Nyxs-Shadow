1b3f8fc1bfe86af2e12708b0b003f69fccf9911e
Chewy <jsilvaisyourbro@gmail.com>
2025-02-10 22:44:53 -0600

    Two hit Combo
    Two hit melee combo has been implemented, second attack only comes out if you're still in attack range when the enemy is doing their first attack, otherwise goes back to chasing the player.

---
+++ Assets/Bosses/Mini-Bosses/Sprites/TreeSpirit/AttackOne.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+
+public class AttackOne : StateMachineBehaviour
+{
+    Transform player;
+    Rigidbody2D rb;
+
+    public float attackRange = 1f;
+
+    // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
+    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+        rb = animator.GetComponent<Rigidbody2D>();
+    }
+
+    // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
+    override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        if (Vector2.Distance(player.position, rb.position) <= attackRange)
+        {
+            animator.SetTrigger("Attack2");
+        }
+    }
+
+    // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
+    override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        animator.ResetTrigger("Attack2");
+    }
+}
