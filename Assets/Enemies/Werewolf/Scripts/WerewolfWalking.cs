ba159ddc1f605525999b280e71af3aea63ca232a
Chewy <jsilvaisyourbro@gmail.com>
2025-02-17 23:06:13 -0600

    Werewolf Patrolling
    I got the werewolf to patrol between two points with rests in between moving, for now, the wolf does not turn around, will fall off of edges if the patrol point is there and moves based on the determined patrol point instead of total distance travelled.

--- Assets/Enemies/Werewolf/Scripts/WerewolfWalking.cs
+++ Assets/Enemies/Werewolf/Scripts/WerewolfWalking.cs
@@ -25,7 +25,7 @@ public class WerewolfWalking : StateMachineBehaviour
     // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
     override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
-        if (Vector2.Distance(rb.position, patrolPoint) > 1)
+        if (Vector2.Distance(rb.position, patrolPoint) >= 1)
         {
             Vector2 target = new Vector2(patrolPoint.x, rb.position.y);
             Vector2 newPos = Vector2.MoveTowards(rb.position, target, patrolSpeed * Time.fixedDeltaTime);
@@ -38,6 +38,7 @@ public class WerewolfWalking : StateMachineBehaviour
     // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
     override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
+        goingRight = !goingRight;
         animator.ResetTrigger("SitIdle");
     }
 }
