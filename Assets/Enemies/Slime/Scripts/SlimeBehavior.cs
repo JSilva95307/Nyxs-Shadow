f536c1172a33f2a7a01be0ece2e70c094ea0ff42
Chewy <jsilvaisyourbro@gmail.com>
2025-02-25 21:23:22 -0600

    Slime Foundation
    Created and imported the slime assets such as the sprites, animtor, prefab and basic behavior script.

---
+++ Assets/Enemies/Slime/Scripts/SlimeBehavior.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+
+public class SlimeBehavior : BaseEnemy
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    public override void Attack()
+    {
+    }
+
+    public override void Attack2()
+    {
+    }
+
+    public override void Attack3()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
