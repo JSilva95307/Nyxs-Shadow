6616e22fcc796de138b4032a60ace4466f1aa082
Chewy <jsilvaisyourbro@gmail.com>
2025-02-27 22:23:38 -0600

    Slime patrol
    Implemented Slime Patrol logic.

--- Assets/Enemies/Slime/Scripts/SlimePatrol.cs
+++ Assets/Enemies/Slime/Scripts/SlimePatrol.cs
@@ -2,33 +2,46 @@ using UnityEngine;
 
 public class SlimePatrol : StateMachineBehaviour
 {
+    public float patrolDist = 5f;
+    public float patrolSpeed = 8f;
+    [SerializeField] Vector2 patrolPoint = Vector2.zero;
+    [SerializeField] bool goingRight = true;
+    Rigidbody2D rb;
+    SlimeBehavior wolf;
+
     // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
-    //override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
-    //{
-    //    
-    //}
+    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        rb = animator.GetComponent<Rigidbody2D>();
+        wolf = animator.GetComponent<SlimeBehavior>();
+        if (goingRight)
+        {
+            patrolPoint = new Vector2(rb.position.x + patrolDist, rb.position.y);
+        }
+        else
+        {
+            patrolPoint = new Vector2(rb.position.x - patrolDist, rb.position.y);
+        }
+        wolf.FlipSlime();
+    }
 
     // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
-    //override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
-    //{
-    //    
-    //}
+    override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        if (Vector2.Distance(rb.position, patrolPoint) >= 1)
+        {
+            Vector2 target = new Vector2(patrolPoint.x, rb.position.y);
+            Vector2 newPos = Vector2.MoveTowards(rb.position, target, patrolSpeed * Time.fixedDeltaTime);
+            rb.MovePosition(newPos);
+        }
+        else
+            animator.SetTrigger("SitIdle");
+    }
 
     // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
-    //override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
-    //{
-    //    
-    //}
-
-    // OnStateMove is called right after Animator.OnAnimatorMove()
-    //override public void OnStateMove(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
-    //{
-    //    // Implement code that processes and affects root motion
-    //}
-
-    // OnStateIK is called right after Animator.OnAnimatorIK()
-    //override public void OnStateIK(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
-    //{
-    //    // Implement code that sets up animation IK (inverse kinematics)
-    //}
+    override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        goingRight = !goingRight;
+        animator.ResetTrigger("SitIdle");
+    }
 }
