4d4e3e6133936b17326022cc33dec680eb61e9d9
Chewy <jsilvaisyourbro@gmail.com>
2025-03-05 22:51:28 -0600

    Defense Zone
    Starting to Implement an enemy-specific object that defines an area for a list of enemies to protect/patrol.

---
+++ Assets/Enemies/Special Objs/Defense Zone/Script/DefenseZone.cs
@@ -0,0 +1,25 @@
+using NUnit.Framework;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class DefenseZone : MonoBehaviour
+{
+    List<GameObject> defendingEnemies;
+    public GameObject defenderType;
+    public GameObject defenderCount;
+
+    UnityEvent playerEntered;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
