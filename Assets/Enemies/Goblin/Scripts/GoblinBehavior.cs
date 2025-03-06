3203c43e05140dffbc7ffc28d556425b408a1917
Chewy <jsilvaisyourbro@gmail.com>
2025-03-05 22:31:56 -0600

    Goblin Prefab
    Added components and variables needed for the goblin to start working with.

--- Assets/Enemies/Goblin/Scripts/GoblinBehavior.cs
+++ Assets/Enemies/Goblin/Scripts/GoblinBehavior.cs
@@ -2,10 +2,26 @@ using UnityEngine;
 
 public class GoblinBehavior : BaseEnemy
 {
+    public BoxCollider2D hitbox;
+    public float damage;
+    public Animator animator;
+
+    private Health health;
+
+    public float playerCheckRange;
+    public LayerMask playerMask;
+    private bool facingRight = true;
+    public bool playerFound;
+
+    RaycastHit2D playerCheck;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        health = GetComponent<Health>();
+        //health.AddDeathListener(PlayDeathAnim);
+        animator.SetBool("Patrol", true);
+        hitbox.enabled = false;
     }
 
     // Update is called once per frame
