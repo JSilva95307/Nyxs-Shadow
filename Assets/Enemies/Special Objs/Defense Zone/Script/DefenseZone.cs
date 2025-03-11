dc0d42986c89bd37b6f683837e61bf7b3196a64e
Chewy <jsilvaisyourbro@gmail.com>
2025-03-10 23:02:54 -0500

    Defense Zone Adjustments
    Made some adjustments to some of the variables that need to be set in the inspecter for the defense zone.

--- Assets/Enemies/Special Objs/Defense Zone/Script/DefenseZone.cs
+++ Assets/Enemies/Special Objs/Defense Zone/Script/DefenseZone.cs
@@ -5,16 +5,20 @@ using UnityEngine.Events;
 
 public class DefenseZone : MonoBehaviour
 {
-    List<GameObject> defendingEnemies;
-    public GameObject defenderType;
-    public GameObject defenderCount;
+    [SerializeField] List<GameObject> defendingEnemies;
+    public int defenderCount;
+    public BaseEnemy enemyType;
 
     UnityEvent<bool> playerUpdate;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-
+        if (defendingEnemies != null)
+        {
+            GenerateSpawnPoints();
+            createDefenders();
+        }
     }
 
     // Update is called once per frame
@@ -25,22 +29,31 @@ public class DefenseZone : MonoBehaviour
 
     private void createDefenders()
     {
-
+        for (int i = 0; i < defenderCount;  ++i)
+        {
+            defendingEnemies[i].GetComponent<BaseEnemy>();
+        }
     }
 
-    private void updateDefenders()
+    private void updateDefenders(bool update)
     {
 
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        
+        if (true)
+            updateDefenders(true);
     }
 
     private void OnCollisionExit2D(Collision2D collision)
     {
-        
+        if(true) 
+            updateDefenders(false);
     }
 
+    private void GenerateSpawnPoints()
+    {
+
+    }
 }
