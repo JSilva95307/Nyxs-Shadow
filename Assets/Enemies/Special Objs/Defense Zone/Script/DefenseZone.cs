3554d224c42405d7401f4ef5afb0e357b1423ae5
Chewy <jsilvaisyourbro@gmail.com>
2025-03-12 22:24:28 -0500

    Defense Spawn Progress
    Decided it might be better to have the enemies already be attatched to the defense zone rather than have them get instantiated during runtime, it's potentially faster and avoids some instantiation problems.

--- Assets/Enemies/Special Objs/Defense Zone/Script/DefenseZone.cs
+++ Assets/Enemies/Special Objs/Defense Zone/Script/DefenseZone.cs
@@ -5,9 +5,11 @@ using UnityEngine.Events;
 
 public class DefenseZone : MonoBehaviour
 {
-    [SerializeField] List<GameObject> defendingEnemies;
+    public List<GameObject> defendingEnemies;
     [UnityEngine.Range(1, 6)] public int defenderCount;
     public BaseEnemy enemyType;
+    BoxCollider2D defenseZone;
+    List<Vector2> spawnPoints;
 
     BaseEnemy curEnemy;
     UnityEvent<bool> playerUpdate;
@@ -15,10 +17,11 @@ public class DefenseZone : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        defenseZone = GetComponent<BoxCollider2D>();
         if (defendingEnemies != null)
         {
             GenerateSpawnPoints();
-            createDefenders();
+            SetDefenderSpawns();
         }
     }
 
@@ -28,9 +31,12 @@ public class DefenseZone : MonoBehaviour
 
     }
 
-    private void createDefenders()
+    private void SetDefenderSpawns()
     {
-        
+        for(int i = 0; i < defenderCount; ++i)
+        {
+            
+        }
     }
 
     private void updateDefenders(bool update)
@@ -52,6 +58,12 @@ public class DefenseZone : MonoBehaviour
 
     private void GenerateSpawnPoints()
     {
-
+        float steps = defenseZone.size.x / defendingEnemies.Count;
+        Vector2 curPos = new Vector2( transform.position.x - defenseZone.size.x, transform.position.y);
+        for(int i = 0; i < defenderCount; ++i)
+        {
+            spawnPoints[i] = curPos;
+            curPos.x += steps;
+        }
     }
 }
