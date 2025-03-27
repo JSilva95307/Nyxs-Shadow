fef2a07e0d22a9a6438d7899b9254432a2ff24e3
Chewy <jsilvaisyourbro@gmail.com>
2025-03-27 09:49:25 -0500

    Tree Spirit Enemy
    Added: foundational parts to the tree spirit enemy including an empty script, prefab, and necessary components.

---
+++ Assets/Enemies/Tree Spirit/Scripts/TreeSpiritEnemy.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+
+public class TreeSpiritEnemy : BaseEnemy
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public override void Attack()
+    {
+    }
+
+    public override void Attack2()
+    {
+    }
+
+    public override void Attack3()
+    {
+
+    }
+}
