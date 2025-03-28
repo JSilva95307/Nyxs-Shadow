401bc15b0b9bb881fa8905487315ea8128c45745
Chewy <jsilvaisyourbro@gmail.com>
2025-03-28 13:05:06 -0500

    TS Checkpoint
    started making some progress on the basic tree spirit enemy. Mainly implemented idle and walk anim clips, also made the projectile, lastly, removed a var from GoblinBehavior that wasn't getting used.

--- Assets/Enemies/Tree Spirit/Scripts/TreeSpiritEnemy.cs
+++ Assets/Enemies/Tree Spirit/Scripts/TreeSpiritEnemy.cs
@@ -2,16 +2,30 @@ using UnityEngine;
 
 public class TreeSpiritEnemy : BaseEnemy
 {
+    private Health health;
+
+    public Animator animator;
+    public float playerCheckRange;
+    public LayerMask playerMask;
+    private bool facingRight = true;
+    public BoxCollider2D meleeCollider;
+    public BaseProjectile thorns;
+
+    RaycastHit2D playerCheck;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        health = GetComponent<Health>();
+        health.AddDeathListener(PlayDeathAnim);
+        meleeCollider.enabled = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        CheckGround();
+        ApplyGravity();
     }
 
     public override void Attack()
@@ -24,6 +38,10 @@ public class TreeSpiritEnemy : BaseEnemy
 
     public override void Attack3()
     {
+    }
 
+    public void PlayDeathAnim()
+    {
+        animator.SetTrigger("Die");
     }
 }
