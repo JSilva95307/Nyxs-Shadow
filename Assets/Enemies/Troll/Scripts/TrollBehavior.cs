2d1f5e7109167c312da2eecb58925070f57222b6
Chewy <jsilvaisyourbro@gmail.com>
2025-04-07 22:54:20 -0500

    Troll Temp Sprites
    Imported some temporary sprite assets for the troll enemy to work while I implement the logic.

--- Assets/Enemies/Troll/Scripts/TrollBehavior.cs
+++ Assets/Enemies/Troll/Scripts/TrollBehavior.cs
@@ -2,10 +2,13 @@ using UnityEngine;
 
 public class TrollBehavior : BaseEnemy
 {
+    public float slapDamage;
+    public float smashDamage;
+    Health health;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        health = GetComponent<Health>();
     }
 
     // Update is called once per frame
