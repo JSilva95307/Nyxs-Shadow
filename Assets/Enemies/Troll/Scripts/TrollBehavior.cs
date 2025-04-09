72e754d932dfc986b5a0310e5cd92e2a309e2fb2
Chewy <jsilvaisyourbro@gmail.com>
2025-04-08 22:30:15 -0500

    Troll Progress
    Created the idle state and walking anim clip, also started to expand on the behavior vars to include some necessary functional pieces moving forwards.

--- Assets/Enemies/Troll/Scripts/TrollBehavior.cs
+++ Assets/Enemies/Troll/Scripts/TrollBehavior.cs
@@ -4,7 +4,18 @@ public class TrollBehavior : BaseEnemy
 {
     public float slapDamage;
     public float smashDamage;
-    Health health;
+    public Animator animator;
+    public float playerCheckRange;
+    public LayerMask playerMask;
+    public BoxCollider2D meleeCollider;
+    public GameObject thorns;
+    public Transform spawnLocation;
+    public int meleeDamage;
+    private Health health;
+    private bool facingRight = true;
+
+    private RaycastHit2D playerCheck;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -14,7 +25,19 @@ public class TrollBehavior : BaseEnemy
     // Update is called once per frame
     void Update()
     {
-        
+        CheckGround();
+        ApplyGravity();
+
+
+        if (targetSet == true && Vector2.Distance(targetLocation, transform.position) > 0.25f)
+        {
+            MoveTo();
+        }
+        else if (targetSet == true && Vector2.Distance(targetLocation, transform.position) <= 0.25f)
+        {
+            targetSet = false;
+            targetLocation = Vector2.zero;
+        }
     }
 
     public override void Attack()
@@ -27,5 +50,20 @@ public class TrollBehavior : BaseEnemy
 
     public override void Attack3()
     {
+    }
+    
+    public void DisableAttack()
+    {
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        
     }
 }
