6ad444f9cd2cc69e613980bb5bef65ae494a582b
Chewy <jsilvaisyourbro@gmail.com>
2025-04-18 22:50:11 -0500

    Bandit Behavior
    Implemented the majority of the Bandit's behavior script, namely: Gravity in the update function, attack dependent functions, a death anim function and a flip bandit function.

--- Assets/Enemies/Bandit/Scripts/BanditBehavior.cs
+++ Assets/Enemies/Bandit/Scripts/BanditBehavior.cs
@@ -14,13 +14,14 @@ public class BanditBehavior : BaseEnemy
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-
+        health = GetComponent<Health>();
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        CheckGround();
+        ApplyGravity();
     }
 
     public override void Attack()
@@ -33,5 +34,45 @@ public class BanditBehavior : BaseEnemy
 
     public override void Attack3()
     {
+    }
+
+    public void DisableAttack()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            collision.GetComponent<Health>().TakeDamage(meleeDamage);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+
+    }
+
+    public void FlipBandit()
+    {
+        Quaternion rotation = transform.rotation;
+        if (facingRight)
+        {
+            rotation.y = 0;
+        }
+        else
+        {
+            rotation.y = 180;
+        }
+        transform.localRotation = rotation;
+        facingRight = !facingRight;
+    }
+    public void LookAtPlayer() { FacePlayer(); }
+    public void BanditDeath() {  }
+
+    public void KnifeSpawn()
+    {
+       
     }
 }
