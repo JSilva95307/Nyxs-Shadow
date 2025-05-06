c5b6ecda4494440f407e481938590730eb8b7547
Chewy <jsilvaisyourbro@gmail.com>
2025-05-05 21:53:00 -0500

    Bandit Run
    Bandit runs away up to a maximum distance away from the player before then waiting for the player to get within range once more before trying to run away again. This way the player has a chance to catch the bandit if they'd run away from a potential group fight.

--- Assets/Enemies/Bandit/Scripts/Bandit_Wait.cs
+++ Assets/Enemies/Bandit/Scripts/Bandit_Wait.cs
@@ -1,35 +1,32 @@
 using UnityEngine;
 
-public class Bandit_Retreat : StateMachineBehaviour
+public class Bandit_Wait : StateMachineBehaviour
 {
-    public float avoidRange = 20f;
-    Transform player;
-    Transform rb;
+    public float playerDetectRange;
+
+    Transform curPos;
+    Transform playerPos;
     BanditBehavior bandit;
     // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
     override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
-        bandit = animator.GetComponent<BanditBehavior>();
-        rb = bandit.GetTransform();
-        player = GameObject.FindGameObjectWithTag("Player").transform;
+        bandit = animator.GetComponent<BanditBehavior>(); 
+        curPos = bandit.GetTransform();
+        playerPos = GameObject.FindGameObjectWithTag("Player").transform;
     }
 
     // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
     override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
-        if (Vector2.Distance(rb.position, player.position) < avoidRange)
+        if(Vector2.Distance(curPos.position, playerPos.position) <= playerDetectRange)
         {
-            bandit.AvoidPlayer();
+            animator.SetTrigger("Rerun");
         }
-        else
-            animator.SetTrigger("Wait");
-        bandit.LookAwayFromPlayer();
-
     }
 
     // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
     override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
-        animator.ResetTrigger("Wait");
+        animator.ResetTrigger("Rerun");
     }
 }
