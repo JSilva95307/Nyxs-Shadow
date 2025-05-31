b1a03c5a18efa9d597f2b2d09ba019d8f8f5a9e4
Chewy <jsilvaisyourbro@gmail.com>
2025-05-30 23:12:25 -0500

    Run Attack Implemented
    Run attack has been fully implemented for now to be triggered based on how far the player stands away from the werewolf enters its run state.

--- Assets/Enemies/Werewolf/Scripts/PlayerHunt.cs
+++ Assets/Enemies/Werewolf/Scripts/PlayerHunt.cs
@@ -3,9 +3,11 @@ using UnityEngine;
 public class PlayerHunt : StateMachineBehaviour
 {
     public float meleeRange;
+    public float runAttackRange;
     Transform player;
     Transform rb;
     WerewolfBehavior wolf;
+    bool shouldMelee;
 
     // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
     override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
@@ -13,19 +15,32 @@ public class PlayerHunt : StateMachineBehaviour
         player = GameObject.FindGameObjectWithTag("Player").transform;
         wolf = animator.GetComponent<WerewolfBehavior>();
         rb = wolf.GetTransform();
+        if (Vector2.Distance(player.position, rb.position) > 10)
+            shouldMelee = false;
+        else
+            shouldMelee = true;
     }
 
     // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
     override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
         wolf.ChasePlayer();
-        if (Vector2.Distance(player.position, rb.position) <= meleeRange)
-            animator.SetTrigger("Melee1");
+        if (shouldMelee)
+        {
+            if (Vector2.Distance(player.position, rb.position) <= meleeRange)
+                animator.SetTrigger("Melee1");
+        }
+        else
+        {
+            if (Vector2.Distance(player.position, rb.position) <= runAttackRange)
+                animator.SetTrigger("RunAttack");
+        }
     }
 
     // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
     override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
         animator.ResetTrigger("Melee1");
+        animator.ResetTrigger("RunAttack");
     }
 }
