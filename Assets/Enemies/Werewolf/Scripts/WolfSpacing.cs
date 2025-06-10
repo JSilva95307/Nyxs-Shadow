f16f2452f8ae68206bafabcd719cbf2f8eb1cfd3
Chewy <jsilvaisyourbro@gmail.com>
2025-06-09 21:58:21 -0500

    Attack Choice
    Attacks for the werewolf are chosen based on how long they consecutively run away or towards the wolf, either doing a lunging attack if they run away or swiping at them if they choose to get closer to them

--- Assets/Enemies/Werewolf/Scripts/WolfSpacing.cs
+++ Assets/Enemies/Werewolf/Scripts/WolfSpacing.cs
@@ -7,6 +7,8 @@ public class WolfSpacing : StateMachineBehaviour
     WerewolfBehavior wolf;
     Transform wolfPos;
     Transform playerPos;
+    float meleeTimer;
+    float runATimer;
 
     // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
     override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
@@ -14,6 +16,8 @@ public class WolfSpacing : StateMachineBehaviour
         wolf = animator.GetComponent<WerewolfBehavior>();
         wolfPos = wolf.GetTransform();
         playerPos = GameObject.FindGameObjectWithTag("Player").transform;
+        meleeTimer = 0;
+        runATimer = 0;
     }
 
     // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
@@ -23,16 +27,27 @@ public class WolfSpacing : StateMachineBehaviour
         if (Vector2.Distance(wolfPos.position, playerPos.position) < minFollowDist)
         {
             wolfPos.Translate(-Vector2.right * (wolf.GetMoveSpeed() * 0.5f) * Time.deltaTime);
+            meleeTimer += Time.fixedDeltaTime;
         }
         if(Vector2.Distance(wolfPos.position, playerPos.position) > maxFollowDist)
         {
             wolfPos.Translate(Vector2.right * (wolf.GetMoveSpeed() * 0.5f) * Time.deltaTime);
+            runATimer += Time.fixedDeltaTime;
+        }
+        if(meleeTimer >= 0.75)
+        {
+            animator.SetTrigger("Melee1");
+        }
+        else if (runATimer >= 0.5)
+        {
+            animator.SetTrigger("RunAttack");
         }
     }
 
     // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
     override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
-
+        animator.ResetTrigger("Melee1");
+        animator.ResetTrigger("RunAttack");
     }
 }
